27649c7fdc5c47dabb89cd2f98cb7bb55cedb48a
Nathan <Nathan@NATHAN-PC>
2021-09-08 22:59:54 -0400

    Task 2

--- umlFoCS/main.cpp
+++ umlFoCS/main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 int main(int argc, char argv[]) {
 
-	list<int> l = {0, 1, 2};
+	list<int> l = {0, 1, 2}; // character
+
+	list<list<int>> mega; // alphabet
+
+
+
 
 
 	return 0;
