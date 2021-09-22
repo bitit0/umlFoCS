a109085ad8a6d30f2626ecd2244b46ff6b2ae03c
Nathan <Nathan@NATHAN-PC>
2021-09-22 18:22:27 -0400

    Changed Task 3 to work with any alphabet

--- umlFoCS/main.cpp
+++ umlFoCS/main.cpp
@@ -7,78 +7,164 @@
 
 using namespace std;
 
-int layer(int n);
+//int layer(int n);
 
 void generateNthString(vector<int> v, int n);
 
-void genBinString(vector<vector<int>>& layer, int n);
+void genBinString(vector<vector<int>>& layer, int n, vector<int> v);
 
 int main() {
 
-	vector<int> v = {0, 1}; // alphabet
+	vector<int> v = {0, 1, 2}; // alphabet
 
 	vector<int> s1; // string
 
-	generateNthString(v, 3);
+	generateNthString(v, 39);
 
 	return 0;
 }
 
-int layer(int n) {
-
-	int i = 0;
-
-	while (n > pow(i, 2)) {
-
-		if (i == 0) {
-			n = n - 1;
-		} else if (i == 1) {
-			n = n - 2;
-		} else {
-			n = n - pow(i, 2);
-		}
-		i++;
-
-	}
-
-	return i;
-
-}
+//int layer(int n, vector<int> v) {
+//
+//	int i = 0;
+//
+//	while (n > pow(i, 2)) {
+//
+//		if (i == 0) {
+//			n = n - 1;
+//		} else if (i == 1) {
+//			n = n - 2;
+//		} else {
+//			n = n - pow(i, 2);
+//		}
+//		i++;
+//
+//	}
+//
+//	return i;
+//
+//}
 
 // e,
 // 0, 1, 
 // 00, 01, 10, 11,
 // 000, 001, 010, 011, 100, 101, 110, 111,
 
-void genBinString(vector<list<int>>& layer, int n) {
+void genBinString(vector<list<int>>& layer, int n, vector<int> v) {
 
 	if (n == 1) return;
 
 	int layerSize = layer.size();
 
+	for (int i = 0; i < v.size()-1; i++) {
+
+		for (int j = 0; j < layerSize; j++) {
+
+			layer.push_back(layer.at(j));
+		}
+
+	}
+
+	layerSize = layer.size();
 
 	for (int i = 0; i < layerSize; i++) {
 
-		layer.push_back(layer.at(i));
+		layer.at(i).push_front(i / (layerSize / v.size()));
 
 	}
+
+	/*for (int i = 0; i < layer.size(); i++) {
+
+		layer.at(i).push_front( (i/v.size()) % v.size());
+
+	}*/
+
+
+	//for (int i = 0; i < layerSize; i++) {
+
+	//	for (int j = 0; j < v.size(); j++) {
+
+	//		list<int> temp(layer.at(i));
+	//		temp.push_back(v.at(j));
+	//		layer.push_back(temp);
+	//		//layer.at(i).push_front(v.at(j));
+	//		cout << "layer size: " << layerSize;
+	//		cout << ", n: " << n << ", i: " << i << ", j: " << j << ", pushing back: " << v.at(j) << endl;
+
+	//	}
+
+	//}
+
+	/*layerSize = layer.size();*/
+
+	/*layerSize = layer.size();*/
+
+	/*int count = 0;
+	while (layer.size() < pow(layerSize, 2)) {
+
+		list<int> temp;
+		temp.push_back(v.at(count % v.size()));
+
+		cout << "current position: " << count << ", pushing: " << v.at(count % v.size()) << endl;
+
+		layer.push_back(temp);
+		count++;
+
+	}
+
+	cout << "layersize wtf: " << layer.size() << endl;
 
 	for (int i = 0; i < layer.size(); i++) {
 
-		if (i < layer.size() / 2)
-			layer.at(i).push_front(0);
-		else
-			layer.at(i).push_front(1);
+		int index = (i % (layer.size() / v.size())) % v.size();
 
-	}
+		cout << "n: " << n << ", i: " << i << ", index: " << index << ", pushing: " << v.at(index) << endl;
+
+		layer.at(i).push_front(v.at((i / v.size()) % v.size()));
+
+	}*/
+
+
+
+	//for (int i = 0; i < layerSize; i++) {
+
+	//	for (int j = 0; j < layerSize; j++) {
+
+	//		layer.push_back(layer.at(j));
+
+	//	}
+
+	//}
+
+	//for (int i = 0; i < layer.size(); i++) {
+
+	//	/*if (i < layer.size() / 2)
+	//		layer.at(i).push_front(0);
+	//	else
+	//		layer.at(i).push_front(1);*/
+
+	//	/*if (i == 0)
+	//	{
+	//		layer.at(i).push_front(v.at(0));
+	//	}
+	//	else {
+
+	//		int push = v.at(i/v.size() % v.size());
+	//		layer.at(i).push_front(push);
+	//	}*/
+
+	//	layer.at(i).push_front(v.at(i/v.size()));
+
+	//}
 	
-	genBinString(layer, n - 1);
-
+	genBinString(layer, n - 1, v);
 
 }
 
 
 void generateNthString(vector<int> v, int n) {
+
+	//cout << "vsize: " << v.size() << endl;
 
 	if (n == 0) {
 
@@ -98,35 +184,62 @@ void generateNthString(vector<int> v, int n) {
 
 	int i = 0; // layer count
 
-	while (n >= pow(2, i)) {
+	//cout << "layer feedback: " << endl;
 
-		n = n - pow(2, i);
+	while (n >= pow(v.size(), i)) {
+
+		//cout << "n: " << n << "; n >= " << v.size() << "^" << i << "?; " << (n >= pow(v.size(), i));
+
+		n = n - pow(v.size(), i);
 		i++;
+
+		//cout << "; new n: " << n << ", new i: " << i << endl;
+
 
 	}
 	//cout << "i: " << i << endl;
 
 	vector<list<int>> layer;
 
-	list<int> f;
+	/*list<int> f;
 	f.push_back(0);
 	list<int> s;
-	s.push_back(1);
-	
-	layer.push_back(f);
-	layer.push_back(s);
+	s.push_back(1);*/
 
-	genBinString(layer, i);
+	for (int j = 0; j < v.size(); j++) {
+
+		list<int> temp;
+		temp.push_back(v.at(j));
+		layer.push_back(temp);
+
+	}
+
+	//layer.push_back(f);
+	//layer.push_back(s);
+
+	genBinString(layer, i, v);
 
 	list<int> temp;
 
-	temp = layer.at(n);
+	//temp = layer.at(n);
 
-	for (auto s : temp) {
+	int testc = 0;
+
+	for (auto s : layer.at(n)) {
+
+		cout << s << "";
+	}
+	cout << endl;
+
+	//cout << "count of elements: " << testc << endl;
+
+	/*cout << "n:" << n;
+
+	for (auto s : layer.at(n)) {
 
 		cout << s;
 
-	}
+	}*/
 	
 
 
