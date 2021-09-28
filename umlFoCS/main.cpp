a263fcda61f59a6b267995daefbf9199b2f01795
Nathan <Nathan@NATHAN-PC>
2021-09-27 22:32:18 -0400

    task 10

--- umlFoCS/main.cpp
+++ umlFoCS/main.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 void generateNthString(vector<int> v, int n);
 
-void genBinString(vector<vector<int>>& layer, int n, vector<int> v);
+void genBinString(vector<list<int>>& layer, int n, vector<int> v);
+
+
 
 template<typename State>
 class DFA {
@@ -27,6 +29,8 @@ public:
 	function<bool(State)> F;
 
 };
+
+bool acceptsString(DFA<char> d, list<int> l);
 
 DFA<char> onlyCharDFA(char in);
 
@@ -193,7 +197,7 @@ int main() {
 				return c == 1 ? 'a' : 'c';
 			}
 			if (qi == 'c') {
-				return c == 1 ? 'd' : 'b';
+				return c == 1 ? 'd' : 'c';
 			}
 			if (qi == 'd') {
 				return c == 1 ? 'e' : 'a';
@@ -228,18 +232,241 @@ int main() {
 		'a',
 		[](char qi, int c) {
 			if (qi == 'a') {
-				return c == 1 ? 'a' : 'b';
+				//return c == 1 ? 'a' : 'b';
+				if (c == 1) return 'a';
+				if (c == 0) return 'b';
 			}
 			if (qi == 'b') {
-				return c == 1 ? 'a' : 'c';
+				//return c == 1 ? 'a' : 'c';
+				if (c == 1) return 'a';
+				if (c == 0) return 'c';
 			}
 			if (qi == 'c') {
-				return c == 1 ? 'a' : 'd';
+				//return c == 1 ? 'a' : 'd';
+				if (c == 1) return 'a';
+				if (c == 0) return 'd';
 			}
 			if (qi == 'd') return 'd';
+			return 'a';
 		},
 		[](char qi) { return (qi == 'd'); }
 		);
+
+	/// //////////////////////// start of tests
+	//////////////////////////////////////////////
+
+	vector<list<int>> accept = { {0, 1}, {0, 1, 1}, {1, 0, 1}, {0, 1, 0, 1}, {1}, {1, 0, 0} };
+	vector<list<int>> reject = { {1, 2}, {2, 3}, {4, 5}, {6, 7}, {8, 10}, {89, 91} };
+
+	int total = 0;
+	int fails = 0;
+
+	// binary string
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*binaryString, c)) {
+			fails++;
+			cout << "binaryString accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*binaryString, c)) {
+			fails++;
+			cout << "binaryString reject fail." << endl;
+		}
+	}
+	// only ones
+
+	accept = { {1, 1}, {1, 1, 1}, {1, 1, 1}, {1, 1, 1, 1}, {1}, {1, 1, 1} };
+	reject = { {1, 0}, {0, 0}, {2, 0}, {0, 7}, {8, 10}, {89, 91} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*onlyOnes, c)) {
+			fails++;
+			cout << "onlyOnes accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*onlyOnes, c)) {
+			fails++;
+			cout << "onlyOnes reject fail." << endl;
+		}
+	}
+
+	// only zeros
+	accept = { {0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0, 0}, {0}, {0, 0, 0} };
+	reject = { {1, 0}, {3, 0}, {2, 0}, {0, 7}, {8, 10}, {89, 91} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*onlyZeros, c)) {
+			fails++;
+			cout << "onlyZeros accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*onlyZeros, c)) {
+			fails++;
+			cout << "onlyZeros reject fail." << endl;
+		}
+	}
+	// alternatingBinary
+
+	accept = { {1, 0}, {1, 0, 1}, {0, 1, 0}, {1, 0, 1, 0}, {0, 1, 0, 1}, {0, 1} };
+	reject = { {1, 2}, {3, 0}, {2, 0}, {0, 7}, {8, 10}, {89, 91} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*alternatingBinary, c)) {
+			fails++;
+			cout << "alternatingBinary accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*alternatingBinary, c)) {
+			fails++;
+			cout << "alternatingBinary reject fail." << endl;
+		}
+	}
+
+	// even length
+	accept = { {1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0, 1, 0}, {1, 0, 1, 0}, {0, 1, 0, 1}, {0, 1} };
+	reject = { {1, 2, 3}, {3, 0, 2}, {2, 8, 0}, {0, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*evenLength, c)) {
+			fails++;
+			cout << "evenLength accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*evenLength, c)) {
+			fails++;
+			cout << "evenLength reject fail." << endl;
+		}
+	}
+	
+	// contains one
+	accept = { {1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0, 1, 0}, {1, 0, 1, 0}, {0, 1, 0, 1}, {0, 1} };
+	reject = { {2, 2, 3}, {3, 0, 2}, {2, 8, 0}, {0, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*containsOne, c)) {
+			fails++;
+			cout << "containsOne accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*containsOne, c)) {
+			fails++;
+			cout << "containsOne reject fail." << endl;
+		}
+	}
+
+	// contains zero
+	accept = { {1, 0}, {1, 0, 0, 0}, {0, 1, 0, 0, 1, 0}, {1, 0, 1, 0}, {0, 1, 0, 1}, {0, 1} };
+	reject = { {2, 2, 3}, {3, 3, 2}, {2, 8, 3}, {3, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*containsZero, c)) {
+			fails++;
+			cout << "containsZero accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*containsZero, c)) {
+			fails++;
+			cout << "containsZero reject fail." << endl;
+		}
+	}
+
+	// contains 0011
+	accept = { {0, 0, 1, 1}, {1, 0, 0, 1, 1}, {1, 0, 0, 1, 1, 1}, {1, 0, 1, 0, 0, 1, 1}, {0, 1, 0, 1, 0, 0, 1, 1}, {0, 1, 0, 1, 1, 0, 0, 0, 1, 1} };
+	reject = { {2, 2, 3}, {3, 0, 2}, {2, 8, 0}, {0, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*contains0011, c)) {
+			fails++;
+			cout << "contains0011 accept fail on ";
+			for (auto w : c) {
+				cout << w;
+			}
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*contains0011, c)) {
+			fails++;
+			cout << "contains0011 reject fail." << endl;
+		}
+	}
+
+	// starts one ends zero
+	accept = { {1, 0}, {1, 0, 0, 0}, {1, 1, 0, 0, 1, 0}, {1, 0, 1, 0}, {1, 1, 0, 0}, {1, 0, 0, 0, 0} };
+	reject = { {2, 2, 3}, {3, 0, 2}, {2, 8, 0}, {0, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*startsOneEndsZero, c)) {
+			fails++;
+			cout << "startsOneEndsZero accept fail on ";
+			for (auto w : c) {
+				cout << w;
+			}
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*startsOneEndsZero, c)) {
+			fails++;
+			cout << "startsOneEndsZero reject fail." << endl;
+		}
+	}
+
+	// three consecutive zeros
+	accept = { {0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {1, 1, 0, 0, 0}, {1, 0, 0, 0, 1}, {1, 1, 1, 0, 0, 0} };
+	reject = { {2, 2, 3}, {3, 0, 2}, {2, 8, 0}, {0, 7, 3, 9, 5}, {8, 10, 11}, {89, 91, 23, 85, 19, 12, 102} };
+
+	for (auto c : accept) {
+		total++;
+		if (!acceptsString(*threeConsecutiveZeros, c)) {
+			fails++;
+			cout << "threeConsecutiveZeros accept fail." << endl;
+		}
+	}
+
+	for (auto c : reject) {
+		total++;
+		if (acceptsString(*threeConsecutiveZeros, c)) {
+			fails++;
+			cout << "threeConsecutiveZeros reject fail on ";
+			for (auto w : c) {
+				cout << w;
+			}
+			cout << endl;
+		}
+	}
 
 	return 0;
 }
@@ -353,4 +580,17 @@ DFA<char> onlyCharDFA(char in) {
 		[](char qi) { return qi == 'a'; }
 		);
 
+}
+
+bool acceptsString(DFA<char> automata, list<int> l) {
+	
+	char qi = automata.q0;
+	for (int curr : l) {
+		
+		qi = automata.D(qi, curr);
+
+
+	}
+
+	return automata.F(qi);
 }
